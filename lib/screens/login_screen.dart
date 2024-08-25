04a3ae7317e28ab3b7013869a00a7afa627fca56
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-08-25 22:16:25 +0530

    created TextFieldInput widget and added email & password section

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,6 +10,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +32,21 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               SvgPicture.asset(
                 'assets/images/ic_instagram.svg',
+                // ignore: deprecated_member_use
                 color: Colors.white,
                 height: 64,
+              ),
+              const SizedBox(height: 64),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: 'Enter password',
+                textInputType: TextInputType.text,
+                isPass: true,
               ),
             ],
           ),
