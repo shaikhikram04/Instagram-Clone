7529174d255d70717a9fb29ab9cd28c5e43c8fdd
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-08-30 22:59:01 +0530

    AddPostScreen UI

--- lib/screens/add_post_sereen.dart
+++ lib/screens/add_post_sereen.dart
@@ -20,6 +20,7 @@ class _AddPostSereenState extends State<AddPostSereen> {
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
         leading: IconButton(
           onPressed: () {},
           icon: const Icon(Icons.arrow_back),
@@ -40,6 +41,48 @@ class _AddPostSereenState extends State<AddPostSereen> {
               ),
             ),
           ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1724762115854-01798ad66178?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write Caption...',
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://images.unsplash.com/photo-1724931498964-a1f392ba9ace?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                        fit: BoxFit.fill,
+                        alignment: FractionalOffset.topCenter,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          )
         ],
       ),
     );
