d07ad6946c79e3516b9b26a302bd9d2b051e67e8
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-10-02 16:14:51 +0530

    rename message to chat

---
+++ lib/models/chat.dart
@@ -0,0 +1,30 @@
+enum MessageType {
+  text,
+  image,
+}
+
+class Chat {
+  Chat({
+    required this.chatId,
+    required this.from,
+    required this.type,
+    required this.message,
+    required this.timeStamp,
+  });
+
+  final String chatId;
+  final String from;
+  final MessageType type;
+  final String message;
+  final DateTime timeStamp;
+
+  Map<String, dynamic> get toJson {
+    return {
+      'chatId': chatId,
+      'from': from,
+      'message': message,
+      'messageType': type.name.toString(),
+      'timeStamp': timeStamp.toString(),
+    };
+  }
+}
