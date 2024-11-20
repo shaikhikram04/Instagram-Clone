c537978334c69c8db0d491062022164f053394b8
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-11-20 22:20:03 +0530

    remove some files

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:device_preview/device_preview.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:instagram_clone/firebase_options.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
@@ -17,20 +17,9 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
   try {
-    if (kIsWeb) {
-      await Firebase.initializeApp(
-        options: FirebaseOptions(
-          apiKey: dotenv.get('web_api_key'),
-          appId: dotenv.get('web_app_id'),
-          messagingSenderId: dotenv.get('messaging_sender_id'),
-          projectId: dotenv.get('project_id'),
-          authDomain: dotenv.get('auth_domain'),
-          storageBucket: dotenv.get('storage_bucket'),
-        ),
-      );
-    } else {
-      Firebase.initializeApp();
-    }
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   } catch (e) {
     print('Error initializing Firebase: $e');
   }
